d579d8a967ec59df5871b9a3255e85bfd7e37eef
Pranshu Kashyap <58984074+pranshukas@users.noreply.github.com>
2021-05-27 16:32:56 +0530

    Update chatbox.js

--- public/js/chatbox.js
+++ public/js/chatbox.js
@@ -1,5 +1,6 @@
 var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date;
 !function () {
     var t = document.createElement("script"), e = document.getElementsByTagName("script")[0];
-    t.async = !0, t.src = "https://embed.tawk.to/608ffb0ab1d5182476b5200d/1f4p6pvg1", t.charset = "UTF-8", t.setAttribute("crossorigin", "*"), e.parentNode.insertBefore(t, e)
+    t.async = !0, t.src = <"Source to Website">, t.charset = "UTF-8", t.setAttribute("crossorigin", "*"), e.parentNode.insertBefore(t, e)
+    //Make Sure to Connect before using !
 }()
